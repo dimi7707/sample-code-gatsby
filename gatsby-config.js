07e3427ccc0ba6e460187a0d344e1b7c359e520e
Antonio Touriño <atourino@rootstack.com>
2020-10-13 23:43:20 -0500

    Connect to Rootstack GraphQL API.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,14 @@
+const activeEnv =  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
+
+console.log(`Using environment config: '${activeEnv}'`)
+
+require("dotenv").config({
+  path: `.env.${activeEnv}`,
+})
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Rootstack`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
@@ -11,6 +19,17 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-graphql`,
+      options: {
+        typeName: "Rootstack",
+        fieldName: "rootstack",
+        url: process.env.DRUPAL_GRAPHQL_API_URL,
+        headers: {
+          Authorization: `Basic: ${Buffer.from(process.env.BASIC_AUTH_USER + ":" + process.env.BASIC_AUTH_PASSWORD).toString("base64")}`
+        },
       },
     },
     `gatsby-transformer-sharp`,
