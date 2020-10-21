926e2dfecc2c929dbad0c31599d10f244cf462fd
Hector Rodriguez <hrodriguez@rootstack.com>
2020-10-21 10:30:21 -0500

    feat(project-structure): add path aliases support

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,5 +3,15 @@
  *
  * See: https://www.gatsbyjs.com/docs/node-apis/
  */
+const path = require('path');
 
-// You can delete this file if you're not using it
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js', '.jsx'],
+      alias: {
+        '@core': path.resolve(__dirname, 'src/core')
+      }
+    }
+  });
+};
--- src/core/layout/layout.tsx
+++ src/core/layout/layout.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useStaticQuery, graphql } from 'gatsby';
 
-import Header from './header';
+import Header from '@core/layout/header';
 import './layout.scss';
 
 const Layout = ({ children }) => {
