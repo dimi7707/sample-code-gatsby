0d8df94e7c71cda24133e518707cf1b6a6bd1c57
Hector Rodriguez <hrodriguez@rootstack.com>
2020-10-21 13:47:03 -0500

    feat(project-structure): add bootstrap support

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,6 +44,11 @@ module.exports = {
         icon: 'src/assets/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
-    'gatsby-plugin-sass',
-  ],
+    {
+      resolve: 'gatsby-plugin-sass',
+      options: {
+        includePaths: ['bootstrap/scss/bootstrap'],
+      }
+    }
+  ]
 };
