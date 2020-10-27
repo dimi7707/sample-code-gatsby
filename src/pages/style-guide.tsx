7222470985517e005bda5859d96646d53467f37b
Dimitri Avila <davila@rootstack.com>
2020-10-27 08:46:22 -0500

    create template for style guide

---
+++ src/pages/style-guide.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import Layout from '../core/layout/layout';
+import Image from '../core/layout/image';
+import SEO from '../core/layout/seo';
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Soy la hoja de estilos</h1>
+    
+  </Layout>
+);
+
+export default IndexPage;
