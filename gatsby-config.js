e10b849da07794a283d5973985d47cd2ff1ff5bc
Dimitri Avila <davila@rootstack.com>
2020-10-28 14:12:18 -0500

    removed commented code

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,8 +55,6 @@ module.exports = {
       options: {
         fonts: [
           `roboto`
-          //`limelight`,
-          //`source sans pro\:300,400,400i,700` // you can also specify font weights and styles
         ],
         display: 'swap'
       }
