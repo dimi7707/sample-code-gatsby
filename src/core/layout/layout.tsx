18fd22ff731a489103a4b1e687ab43c2395e9d36
Dimitri Avila <davila@rootstack.com>
2020-11-09 16:50:14 -0500

    linter fixes

--- src/core/layout/layout.tsx
+++ src/core/layout/layout.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import { useStaticQuery, graphql } from 'gatsby';
-
 import './layout.scss';
 
 type HeroProps = {
@@ -9,18 +7,6 @@ type HeroProps = {
 };
 
 export default function Layout({ content }: HeroProps): React.ReactElement {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
-
-  const title = data.site.siteMetadata?.title || 'Title';
-
   return (
     <>
       <div>
