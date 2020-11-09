6c078c6e6fb4a9ff566339ba2bc475cbfc0279b6
Dimitri Avila <davila@rootstack.com>
2020-11-09 15:51:54 -0500

    refactorization, using HOC

--- src/core/providers/image-provider.tsx
+++ src/core/providers/image-provider.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import Img from 'gatsby-image';
-import PropTypes from 'prop-types';
 
-const ImageProvider = ({ fileName, alt }) => {
+type imageProviderProps = {
+  fileName: string;
+  alt: string;
+};
+
+export default function ImageProvider({ fileName, alt }: imageProviderProps) {
   const { allImageSharp } = useStaticQuery(graphql`
     query {
       allImageSharp {
@@ -22,11 +26,4 @@ const ImageProvider = ({ fileName, alt }) => {
       <Img fluid={fluid} alt={alt} />
     </figure>
   );
-};
-
-ImageProvider.propTypes = {
-  fileName: PropTypes.string.isRequired,
-  alt: PropTypes.string.isRequired
-};
-
-export default ImageProvider;
+}
