50201ac7334a2863528ce6c480564faa3896bd82
Dimitri Avila <davila@rootstack.com>
2020-11-17 16:05:24 -0500

    refactoring

--- src/core/layout/footer/footer.tsx
+++ src/core/layout/footer/footer.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
 import ImageProvider from '@core/providers/image/image-provider';
-
-import { SocialIconProps } from '@shared/props/social-icon';
-
+import { SocialIcon } from '@shared/models/social-icon';
 import { FooterProps } from '@props/footer';
 
 import './footer.scss';
@@ -21,7 +19,7 @@ export default function Footer(): React.ReactElement {
     ]
   } as FooterProps;
 
-  const socialIcons = footerData.socialIconList.map((icon: SocialIconProps) => (
+  const socialIcons = footerData.socialIconList.map((icon: SocialIcon) => (
     <a href={icon.link} key={`social-${icon.icon}`}>
       <ImageProvider fileName={icon.icon} className="" alt="" />
     </a>
@@ -36,29 +34,31 @@ export default function Footer(): React.ReactElement {
             <div className="footer-social-icons">
               {socialIcons}
             </div>
-            <p className="footer-text-copyright">Copyright © 2017 Rootstack. All Rights Reserved.</p>
+            <p className="footer-text-copyright small">
+              Copyright © 2017 Rootstack. All Rights Reserved.
+            </p>
           </Col>
           <hr className="clearfix w-100 d-md-none pb-3" />
           <Col className="col-md-3 mb-md-0 mb-3">
             <h5 className="text-uppercase">Panamá</h5>
             <ul className="list-unstyled">
-              <li>IPA Street. House #10, Hato Pintado,</li>
-              <li>Panama City, Panama.</li>
+              <li className="small">IPA Street. House #10, Hato Pintado,</li>
+              <li className="small">Panama City, Panama.</li>
             </ul>
             <ul className="list-unstyled">
-              <li>+507 209 9002</li>
-              <li>+507 399 0129</li>
+              <li className="small">+507 209 9002</li>
+              <li className="small">+507 399 0129</li>
             </ul>
           </Col>
           <Col className="col-md-3 mb-md-0 mb-3">
             <h5 className="text-uppercase">USA</h5>
             <ul className="list-unstyled">
-              <li>4241 John&apos;s Light Dr. </li>
-              <li>Austin, Texas. </li>
-              <li>78727. USA.</li>
+              <li className="small">4241 John&apos;s Light Dr. </li>
+              <li className="small">Austin, Texas. </li>
+              <li className="small">78727. USA.</li>
             </ul>
             <ul className="list-unstyled">
-              <li> +1 215-883-4359</li>
+              <li className="small"> +1 215-883-4359</li>
             </ul>
           </Col>
         </Row>
--- src/shared/models/social-icon.ts
+++ src/shared/models/social-icon.ts
@@ -1,4 +1,4 @@
-export type SocialIconProps = {
+export type SocialIcon = {
   icon: string;
   link: string;
 };
