e639b81e40993c98540ea52ae4c958a6a537313a
Dimitri Avila <davila@rootstack.com>
2020-11-19 16:17:17 -0500

    working on demo 1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,19 +37,27 @@ const cardsContent = entries.map((entry: CardProps, index: number) => (
 export default function IndexPage(): React.ReactElement {
   return (
     <div className="mb-5">
-      <SEO title="Home" />
-
-      <Container>
-        <h3 className="text-center">We Deliver</h3>
-        <IntroText
-          text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor ut fugit
-          repudiandae quia assumenda dolores labore, repellat modi aliquid minima consequatur
-          corporis odio odit voluptatibus."
-        />
-        <Row>
-          {cardsContent}
-        </Row>
-      </Container>
+      <h1>Demo 19 - 11 - 2020</h1>
+      <ul>
+        <li>
+          <a href="/home">Home</a>
+        </li>
+        <li>
+          <a href="/service">Services</a>
+        </li>
+        <li>
+          <a href="/profile-manage-teams">Profile Manage teams</a>
+        </li>
+        <li>
+          <a href="/profile-staff-augmentation">Profile staff augmentation</a>
+        </li>
+        <li>
+          <a href="/profile-project-based">Profile project based</a>
+        </li>
+        <li>
+          <a href="/success-stories">Success stories</a>
+        </li>
+      </ul>
     </div>
   );
 }
