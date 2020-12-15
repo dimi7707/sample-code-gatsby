f952ca10f5417116b6e51247375e4581f32aaaa3
Dimitri Avila <davila@rootstack.com>
2020-12-15 14:20:46 -0500

    linter fixes

--- src/templates/service.tsx
+++ src/templates/service.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Container, Row, Col } from 'react-bootstrap';
 import { graphql } from 'gatsby';
 
 import TopBar from '@core/layout/top-bar/top-bar';
