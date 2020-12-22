b58a4b74b9427e4dafa29a1612b5e55876eb1a8f
Dimitri <davila@rootstack.com>
2020-12-22 11:53:54 -0500

    clean code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
 
 import TopBar from '@core/layout/top-bar/top-bar';
 import NavigationBar from '@core/layout/navigation-bar/navigation-bar';
 import Hero from '@shared/hero/hero';
 import IntroText from '@shared/text/intro-text';
-import Card from '@shared/card/card';
-import WYSWYG from '@shared/wyswyg/wyswyg';
-import Footer from '@core/layout/footer/footer';
 
 export default function IndexPage(): React.ReactElement {
   const textForIntro = `We provide end-to-end development services working with the model that
@@ -15,7 +11,7 @@ export default function IndexPage(): React.ReactElement {
   providing the necessary talent to complete your team, we guarantee the most efficient
   workflow and exceptional results.`;
   return (
-    <div className="servicepage">
+    <div className="home-page">
       <TopBar
         currentLanguage="en"
         urlCurrentVersion="/"
@@ -36,60 +32,6 @@ export default function IndexPage(): React.ReactElement {
       <div className="mt-4 pt-4 pb-4">
         <IntroText sideDecoration text={textForIntro} />
       </div>
-      <div className="pt-4 mt-4">
-        <WYSWYG
-          title="Benefits"
-          textContent={`Do you need a team of qualified experts to lead and execute your next
-            technology project? Rootstack quickl
-            y takes care of the selection process from start
-            to finish puts together the team that meets your specific tech profile requirements.
-            This way, you can execute your project the way you need it with teams that work
-            autonomously but communicate on your schedule. Access English-fluent talent and senior
-            talent that works efficiently to deliver high-quality solutions
-          `}
-          image="img-profile-manage.png"
-          imageToRight
-          backgroundColor={{ background: 'transparent' }}
-        />
-      </div>
-      <Container className="mt-4 pt-4 mb-4 pb-4">
-        <h4 className="text-center pb-4">Success stories</h4>
-        <Row>
-          <Col xs={12} md={3}>
-            <Card
-              title="Minfo"
-              description="We built an innovative web app and a responsive website for KYC laws"
-              image="img-for-succes-stories.png"
-              link="/"
-            />
-          </Col>
-          <Col xs={12} md={3}>
-            <Card
-              title="Minfo"
-              description="We built an innovative web app and a responsive website for KYC laws"
-              image="img-for-succes-stories.png"
-              link="/"
-            />
-          </Col>
-          <Col xs={12} md={3}>
-            <Card
-              title="Minfo"
-              description="We built an innovative web app and a responsive website for KYC laws"
-              image="img-for-succes-stories.png"
-              link="/"
-            />
-          </Col>
-          <Col xs={12} md={3}>
-            <Card
-              title="Minfo"
-              description="We built an innovative web app and a responsive website for KYC laws"
-              image="img-for-succes-stories.png"
-              link="/"
-            />
-          </Col>
-        </Row>
-      </Container>
-      <Footer />
     </div>
   );
 }
--- src/templates/landing.tsx
+++ src/templates/landing.tsx
@@ -5,59 +5,17 @@ import TopBar from '@core/layout/top-bar/top-bar';
 import NavigationBar from '@core/layout/navigation-bar/navigation-bar';
 import Footer from '@core/layout/footer/footer';
 
-import ComponentIdentifier from '@templates/util-templates/component-identifier';
-
-export default function LandingTemplate({ data }: any) : React.ReactElement {
-  const mainContent = data.nodePage.relationships.field_content;
-  const contentToRender = [];
-  mainContent.forEach((elementContent) => {
-    contentToRender.push(
-      ComponentIdentifier(
-        elementContent.relationships.paragraph_type.label,
-        elementContent.id, mainContent
-      )
-    );
-  });
-
+export default function LandingTemplate() : React.ReactElement {
   return (
     <div>
       <TopBar
-        currentLanguage={data.nodePage.path.langcode}
-        urlCurrentVersion={`${data.nodePage.path.langcode}${data.nodePage.path.alias}`}
-        urlTranslateVersion={data.nodePage.field_slug_translate_version}
+        currentLanguage="es"
+        urlCurrentVersion="/"
+        urlTranslateVersion="/"
       />
       <NavigationBar />
-      {contentToRender.map((content) => (content))}
+        <p>Dummy content</p>
       <Footer />
     </div>
   );
 }
-
-export const query = graphql`
-  query($id: String!) {
-    nodePage: nodePage(id: { eq: $id }) {
-      body {
-        processed
-        format
-      }
-      path {
-        alias
-        langcode
-      }
-      relationships {
-        node_type {
-          description
-          name
-          relationships {
-            node__article {
-              body {
-                processed
-              }
-            }
-          }
-        }
-      }
-      title
-    }
-  }
-`;
